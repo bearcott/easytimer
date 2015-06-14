781d377c762fd0fe9f0b0e76814a6e52f3c5ffb3
Bear Cott <bear@bearcott.com>
2015-06-14 05:07:36 -0500

    added brightness option

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,41 @@
 
-$(function() { 
 //clock class
-	var Clock = function(container) {
-		this.container = container;
-		this.hour = this.container.find('.hour');
-		this.minute = this.container.find('.minute');
-		this.second = this.container.find('.second');
-		this.ampm = this.container.find('.ampm');
-		this.refresh = function() {
-			var date = new Date();
-			if (date.getHours() > 12) {
-				this.hour.html(date.getHours() - 12);
-				this.ampm.html("pm");
-			} else {
-				this.hour.html(date.getHours());
-				this.ampm.html("am");
-			}
-
-			this.minute.html((date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes());
-			this.second.html((date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds());
+var Clock = function(container) {
+	this.container = container;
+	this.hour = this.container.find('.hour');
+	this.minute = this.container.find('.minute');
+	this.second = this.container.find('.second');
+	this.ampm = this.container.find('.ampm');
+	this.refresh = function() {
+		var date = new Date();
+		if (date.getHours() > 12) {
+			this.hour.html(date.getHours() - 12);
+			this.ampm.html("pm");
+		} else {
+			this.hour.html(date.getHours());
+			this.ampm.html("am");
 		}
-	}
 
+		this.minute.html((date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes());
+		this.second.html((date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds());
+	}
+}
+
+
+$(function() {
 	var clock = new Clock($('.clock'));
+
+	//tick tock
 	var tick = setInterval(function() {
 		clock.refresh();
 	}, 100);
 
-	//brightness
+	//set brightness by time of day
+	var tod = (new Date()).getHours();
+	if (tod > 8 && tod < 20)
+		$('body').addClass('light');
+
+	//switch brightness
 	$('#brightness').click(function() {
 		if ($('body').hasClass('light'))
 			$('body').removeClass('light')
