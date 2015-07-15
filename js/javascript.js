0b2b67472159752d152657cabcbc4c7d9860a31e
Bear Cott <bear@bearcott.com>
2015-07-14 23:05:42 -0500

    added menu options

--- js/javascript.js
+++ js/javascript.js
@@ -38,7 +38,12 @@ var Clock = function(container) {
 
 
 $(function() {
-	var clock = new Clock($('.clock'));
+	var clock = new Clock($('#clock'));
+
+	//set default mode to clock
+	$('.mode').hide();
+	$('#clock').show();
+
 
 	//tick tock
 	var tick = setInterval(function() {
@@ -59,7 +64,7 @@ $(function() {
 	})
 
 	//Set Time Type
-	$('.clock').click(function() {
+	$('#clock').click(function() {
 		$this = $(this);
 		if ($this.hasClass('one')) {
 			$this.removeClass('one');
@@ -72,6 +77,26 @@ $(function() {
 			$this.addClass('one');
 		}
 
+	});
+
+	$('#togglemenu').click(function() {
+		if ($('#menu').hasClass('active'))
+			$('#menu').removeClass('active').stop().slideUp(200);
+		else
+			$('#menu').addClass('active').stop().slideDown(200);
+	});
+	$('#menu ul li.btn').click(function() {
+		if ($(this).hasClass('selected'))
+			return;
+		$('#menu ul li.btn').removeClass('selected');
+		$(this).addClass('selected');
+		if ($(this).hasClass('clock')) {
+			$('.mode').stop().fadeOut(200);
+			$('#clock').stop().delay(200).fadeIn(200);
+		}else if ($(this).hasClass('timer')) {
+			$('.mode').stop().fadeOut(200);
+			$('#timer').stop().delay(200).fadeIn(200);
+		}
 	})
 
 });
